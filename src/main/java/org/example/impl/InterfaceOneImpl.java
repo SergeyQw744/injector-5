9972c4cfebed7c0699424c12ef439f697127748b
111 <sergey.streltsov2004@mail.ru>
2024-11-30 23:52:47 +0300

    2

---
+++ src/main/java/org/example/impl/InterfaceOneImpl.java
@@ -0,0 +1,10 @@
+package org.example.impl;
+
+import org.example.interfaces.InterfaceOne;
+
+public class InterfaceOneImpl implements InterfaceOne {
+    @Override
+    public void doSomeOne() {
+        System.out.println("A");
+    }
+}
---
+++ src/main/java/org/example/impl/InterfaceTwoImpl.java
@@ -0,0 +1,10 @@
+package org.example.impl;
+
+import org.example.interfaces.InterfaceTwo;
+
+public class InterfaceTwoImpl implements InterfaceTwo {
+    @Override
+    public void doSomeTwo() {
+        System.out.println("B");
+    }
+}
