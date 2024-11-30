ef5b57a4da54370714b8aade645b4dc9f03a4182
111 <sergey.streltsov2004@mail.ru>
2024-12-01 01:18:20 +0300

    3

--- src/main/java/org/example/beans/SomeBean.java
+++ src/main/java/org/example/beans/SomeBean.java
@@ -1,5 +1,18 @@
 package org.example.beans;
 
+import org.example.annotation.AutoInjectable;
+import org.example.interfaces.InterfaceOne;
+import org.example.interfaces.InterfaceTwo;
+
 public class SomeBean {
+    @AutoInjectable
+    private InterfaceOne oneField;
+    @AutoInjectable
+    private InterfaceTwo twoField;
+
+    public void process(){
+        oneField.doSomeOne();
+        twoField.doSomeTwo();
+    }
 }
 
