f869d1e7a9b2d3904bfc6e6a7926c29cbe222b2a
111 <sergey.streltsov2004@mail.ru>
2024-12-01 16:14:09 +0300

    5

--- src/main/java/org/example/annotation/AutoInjectable.java
+++ src/main/java/org/example/annotation/AutoInjectable.java
@@ -9,6 +9,7 @@ import java.lang.annotation.Target;
  * Интерфейс AutoInjectable
  * Этот интерфейс является аннотацией и служит для внедрения зависимости в
  * объект любого класса через поле.
+ * Поля с повешенной этой аннотацией должны быть инъектированы.
  */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
